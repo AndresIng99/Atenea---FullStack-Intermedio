cf12d0dcc3411a4e2f538d04bb2f76ddb8c93fac
AndresPineda <soporte1.rita@correo.udistrital.edu.co>
2023-03-07 19:23:55 -0500

    clase_7 - parte 2

---
+++ clase_7/ejercicio_tema/js/functions.js
@@ -0,0 +1,6 @@
+function cambiarClaro() {
+    document.getElementById('filtro').className += "filtro_light"
+}
+function cambiarDark() {
+    document.getElementById("filtro").className =document.getElementById("filtro").className.replace("filtro_Dark");
+}

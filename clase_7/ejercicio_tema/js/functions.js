23edae92084acc191893d711fb7de900a4fdbcbb
AndresPineda <soporte1.rita@correo.udistrital.edu.co>
2023-03-09 17:49:18 -0500

    recursos de pwa

--- clase_7/ejercicio_tema/js/functions.js
+++ clase_7/ejercicio_tema/js/functions.js
@@ -2,5 +2,5 @@ function cambiarClaro() {
     document.getElementById('filtro').className += "filtro_light"
 }
 function cambiarDark() {
-    document.getElementById("filtro").className =document.getElementById("filtro").className.replace("filtro_Dark");
+    document.getElementById("filtro").className += "filtro_Dark";
 }

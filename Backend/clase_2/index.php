4299511442ce91f150133a1687579a317cfdf590
AndresPineda <soporte1.rita@correo.udistrital.edu.co>
2023-04-28 19:32:01 -0500

    clase 2 backend

---
+++ Backend/clase_2/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hola mundo!!</h1>
+    
+    <?php 
+    //Esto es un comentarion de una sola linea 
+    /*Esto es 
+    un comentario
+    de multiples
+    lineas*/
+        const CONSTANTE = 5;
+        $CONSTANTE = 3;
+            $a = 10;
+
+            while ($a >= 0) {
+                echo $a.'</br>';
+                $a--;
+            }
+        
+    ?>
+</body>
+</html>

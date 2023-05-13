3ac4addf9ec529381e1ccfe0f6c3cfde86dd4b8a
AndresPineda <soporte1.rita@correo.udistrital.edu.co>
2023-05-12 19:37:31 -0500

    base64 encode

--- Backend/proyecto/back/register.php
+++ Backend/proyecto/back/register.php
@@ -7,8 +7,10 @@ if (isset($_POST['registro'])) {
     $correo = $_POST['correo'];
     $contra = $_POST['contra'];
 
+    $contra_en = base64_encode($contra);
+
     $sql = mysqli_query($conexion,"INSERT INTO usuarios (nombre, correo, clave) VALUES
-    ('$nombre', '$correo', '$contra')");
+    ('$nombre', '$correo', '$contra_en')");
 
      header ('location:../index.html');
 }
--- Backend/proyecto/back/validar.php
+++ Backend/proyecto/back/validar.php
@@ -5,16 +5,20 @@ include '../db/conexion.php';
 if (isset($_POST['inicio'])) {
     $correo = $_POST['correo'];
     $contra = $_POST['contra'];
+    $contra_enc = base64_encode($contra);
 
     $consulta = mysqli_query($conexion,"SELECT correo, clave from usuarios 
-    where correo = '$correo' AND clave ='$contra'");
+    where correo = '$correo' AND clave = '$contra_enc'");
 
     $cant = mysqli_num_rows($consulta);
 
-    echo $cant;
-
-    
+    if ($cant == 1) {
+        header('location:../aplicativo/home.php');
+    }else{
+        header('location:../index.html');
+    }
 
 }
+#0' or 1 = '1
 
 ?>

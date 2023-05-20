5a099e28619009db93d08ec2475121a13e110ac9
AndresPineda <soporte1.rita@correo.udistrital.edu.co>
2023-05-19 19:27:45 -0500

    ejercicio de arreglos, matrices y variables

--- Backend/proyecto/arrays.php
+++ Backend/proyecto/arrays.php
@@ -26,26 +26,38 @@
 if (isset($_POST['datos'])) {
 $filas = $_POST['filas'];
 $columnas = $_POST['columnas'];
-
-
 echo '<form action="" method="POST">
     <table border="1">';
-
     //filas = 3 - columnas = 2 
     for($i=1; $i<=$filas; $i++){
-
         echo "<tr>";
-
         for($x=1; $x<=$columnas; $x++){
-            echo '<td><input type="number"></td>';
+            echo '<td><input type="number" name="table'.$i.'-'.$x.'"></td>
+            ';
         }
         echo '</tr>';
-
     }
     echo '</table>
-</form>
-';
+        <input type="hidden" name="filas" value="'.$filas.'">
+        <input type="hidden" name="columnas" value="'.$columnas.'">
+        <input type="submit" value="Mostrar" name="view_data">
+        </form>
+    ';
 }
+if (isset($_POST['view_data'])) {
+    $filas2 = $_POST['filas'];
+    $columnas2 = $_POST['columnas'];
+    echo '<table border="1">';
+        for($i=1; $i<=$filas2; $i++){
+            echo "<tr>";
+            for($x=1; $x<=$columnas2; $x++){
+                echo '<td>'.$_POST['table'.$i.'-'.$x].'</td>
+                ';
+            }
+            echo '</tr>';
+        }
+        echo '</table>';
+    }
 ?>
 
 </body>
--- Backend/proyecto/arrays2.php
+++ Backend/proyecto/arrays2.php
@@ -6,12 +6,12 @@ $carros[1][0] = "Camila";
 $carros[1][1] = "Luis"; 
 
 $cars = array (
-            array("Volvo"     ,22,18,2),
-            array("BMW"       ,15,13,6),
-            array("Saab"      ,5 ,2,9),
-            array("chevrolet"      ,15 ,12,92)
+            array(1,5,0,1),
+            array(2,8,3,2),
+            array(0,4,2,0),
+            array(1,0,2,1)
         );
 
-echo $cars[1][3];
+echo $cars[2][3];
 
 ?>
